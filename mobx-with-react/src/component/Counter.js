b10850374715d2cf206556f2d35fc392571705e2
jaco <fbwotjq@gmail.com>
2018-12-22 22:09:29 +0900

    스토어에 특정 부분만 이렇게 받을수도 있다.

--- mobx-with-react/src/component/Counter.js
+++ mobx-with-react/src/component/Counter.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react';
 
-@inject('counter')
+//@inject('counter')
+@inject(stores => ({
+    number: stores.counter.number,
+    increase: stores.counter.increase,
+    decrease: stores.counter.decrease,
+}))
 @observer
 class Counter extends Component {
+    /*
     render() {
         const { counter } = this.props;
         return (
@@ -11,6 +17,17 @@ class Counter extends Component {
                 <h1>{counter.number}</h1>
                 <button onClick={counter.increase}>+1</button>
                 <button onClick={counter.decrease}>-1</button>
+            </div>
+        );
+    }
+    */
+    render() {
+        const { number, increase, decrease } = this.props;
+        return (
+            <div>
+                <h1>{number}</h1>
+                <button onClick={increase}>+1</button>
+                <button onClick={decrease}>-1</button>
             </div>
         );
     }
