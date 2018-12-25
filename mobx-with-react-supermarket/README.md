cd69ff1f81445bb1e5112e4e2ed54f468120b8ad
jaco <fbwotjq@gmail.com>
2018-12-25 12:57:13 +0900

    mobx-react로 슈퍼마켓 앱 만들기

---
+++ mobx-with-react-supermarket/README.md
@@ -0,0 +1,2 @@
+# mobx-with-react - 슈퍼마켓 예제 구현
+console> npm install && npm run start
---
+++ mobx-with-react-supermarket/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'mobx-react';
+import App from './App';
+import RootStore from './stores';
+
+const root = new RootStore(); // *** 루트 스토어 생성
+
+ReactDOM.render(
+    <Provider {...root}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
